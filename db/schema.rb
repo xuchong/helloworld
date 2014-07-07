3e15460605bbc863551f182f793e3e76a1e71f29
wxping <wxping715@gmail.com>
2014-07-07 09:50:52 +0800

    update database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705054158) do
+ActiveRecord::Schema.define(version: 20140707013744) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140705054158) do
     t.integer  "questionnaire_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ip"
   end
 
   create_table "users", force: true do |t|
